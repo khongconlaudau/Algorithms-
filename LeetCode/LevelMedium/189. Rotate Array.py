4702bc9545b07408889cffd15c040c41ed2f3368
Mason Van <vanducmanh266@gmail.com>
2025-05-29 14:36:16 -0400

    An easy solution but time complexity is high

--- LeetCode/LevelMedium/189. Rotate Array.py
+++ LeetCode/LevelMedium/189. Rotate Array.py
@@ -1,15 +1,7 @@
 from typing import List
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        length = len(nums) -1 
-
-        for i in range(k , -1, -1):
-            nums[i], nums[length] = nums[length], nums[i]
-            length -= 1
-
-nums = [99,-1,-100,3]
-sol = Solution()
-sol.rotate(nums,2)
-
-print(nums)
+        for i in range(k):
+            nums.insert(0, nums.pop())
+            
         
